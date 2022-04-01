979945abe30a24f4deca13d90e2140c9e192b4ff
fernandogs07 <fernandogs07@hotmail.com>
2022-04-01 14:28:40 -0300

    alterar caracteres inválidos

--- condicao.js
+++ condicao.js
@@ -1,11 +1,11 @@
 function verificar(){
     var age = document.getElementById("idade").value;
     if(age == ""){
-        alert("Campo idade n„o pode ficar vazio")
+        alert("Campo idade n√£o pode ficar vazio")
     }else if(age < 17){
-        document.getElementById("resultado").innerHTML = "N„o pode votar"
+        document.getElementById("resultado").innerHTML = "N√£o pode votar"
     }else{
-        document.getElementById("resultado").innerHTML = "J· pode votar"
+        document.getElementById("resultado").innerHTML = "J√° pode votar"
     }
 }
 
